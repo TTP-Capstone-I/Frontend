a8025c981c6f5b55a6ddc0e9bcd43888c45a5d5f
Shun Lei Hlaing <shunlei@mac.bmcc.cuny.edu>
2026-07-23 15:17:56 -0400

    created Vote Page

--- src/pages/ViewPoll.jsx
+++ src/pages/ViewPoll.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams, Link} from 'react-router'
+import { useParams, Link, useNavigate} from 'react-router'
 import axios from 'axios'
 
 function PollDetails(){
@@ -7,6 +7,10 @@ function PollDetails(){
     const [poll, setPoll] = useState(null)
     const [loading, setLoading] = useState(true)
     const [ error, setError ] = useState('')
+    const [ selectedOption, setSelectedOption ] = useState(null)
+    const [submitting, setSubmitting] = useState(false)
+    const [submitError, setSubmitError] = useState('')
+    const navigate = useNavigate()
 
     const URL = import.meta.env.VITE_API_URL
 
@@ -34,6 +38,30 @@ function PollDetails(){
         getPoll()
     }, [id])
 
+    function handleSelect(optionId){
+        setSelectedOption(optionId)
+    }
+
+    async function handleVote(){
+        if (!selectedOption) return
+        setSubmitting(true)
+        setSubmitting('')
+        try{
+            const response = await axios.post(URL + `/votes` , {
+                optionId : selectedOption
+            })
+            if(!response){
+                throw new Error("Failed to add vote!", response.status)
+            }
+            navigate(`/results/${poll.id}`)
+        }catch(error){
+            console.log(error)
+            setSubmitError(error)
+        }finally{
+            setSubmitting(false)
+        }
+    }
+
     if (loading) return <p> Loading Poll...</p>
     if (error) return <p> Error: {error.message}</p>
     if (!poll) return <p> Poll not found </p>
@@ -46,10 +74,31 @@ function PollDetails(){
             <ul style={{ listStyle: 'none', padding: 0 }}>
                 {poll.options.map((option) => (
                     <li key={option.id}>
-                        <button>{option.title}</button>
+                        <button
+                            onClick={() => handleSelect(option.id)}
+                            style={{
+                                border: option.id === selectedOption
+                                    ? '2px solid dodgerblue'
+                                    : '1px solid gray'
+                            }}
+                        >
+                            {option.title}
+                        </button>
                     </li>
                 ))}
             </ul>
+            <button 
+                onClick={handleVote} 
+                disabled={!selectedOption || submitting}
+                style={{
+                    padding: '4px 10px',
+                    fontSize: '0.85rem',
+                    alignSelf: 'center'
+                }}
+            >
+                {submitting ? 'Submitting...' : 'Vote'}
+            </button>
+                
         </>
     )
 }
